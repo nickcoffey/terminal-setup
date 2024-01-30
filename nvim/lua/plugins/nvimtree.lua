89f299c58e496d8cfa055d18bf704614956280c1
Nick Coffey <nicholasjcoffey@gmail.com>
2024-01-30 10:39:52 -0600

    moved plugin config to respective files

--- nvim/lua/plugins/nvimtree.lua
+++ nvim/lua/plugins/nvimtree.lua
@@ -5,5 +5,7 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("nvim-tree").setup({})
+		local nvim_tree_api = require("nvim-tree.api")
+		vim.keymap.set("n", "<leader>tf", nvim_tree_api.tree.toggle, { desc = "toggle file tree" })
 	end,
 }

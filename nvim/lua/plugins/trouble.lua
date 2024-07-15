045c87910dbdd5eda45202fd70ce4a1a60d970e9
Nick Coffey <nicholasjcoffey@gmail.com>
2024-07-15 11:14:59 -0500

    fixed trouble not showing

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -2,8 +2,7 @@ return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		vim.keymap.set("n", "<leader>te", function()
-			require("trouble").toggle()
-		end, { desc = "[T]oggle Code [E]rrors" })
+		require("trouble").setup({})
+		vim.keymap.set("n", "<leader>te", "<cmd>Trouble diagnostics toggle<CR>", { desc = "[T]oggle Code [E]rrors" })
 	end,
 }

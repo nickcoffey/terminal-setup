809984fa470b44e76e0e0ccc551b4b81e90f3afb
Nick Coffey <nicholasjcoffey@gmail.com>
2024-07-24 10:49:20 -0500

    updated which-key mapping to new spec

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -4,21 +4,21 @@ return {
 	opts = {},
 	config = function()
 		-- document existing key chains
-		require("which-key").register({
-			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-			["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-			["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
-			["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
-			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-			["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-			["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+		require("which-key").add({
+			{ "<leader>c", desc = "[C]ode" },
+			{ "<leader>d", desc = "[D]ocument" },
+			{ "<leader>g", desc = "[G]it" },
+			{ "<leader>h", desc = "Git [H]unk" },
+			{ "<leader>r", desc = "[R]ename" },
+			{ "<leader>s", desc = "[S]earch" },
+			{ "<leader>t", desc = "[T]oggle" },
+			{ "<leader>w", desc = "[W]orkspace" },
 		})
 		-- register which-key VISUAL mode
 		-- required for visual <leader>hs (hunk stage) to work
-		require("which-key").register({
-			["<leader>"] = { name = "VISUAL <leader>" },
-			["<leader>h"] = { "Git [H]unk" },
+		require("which-key").add({
+			{ "<leader>",  desc = "VISUAL <leader>" },
+			{ "<leader>h", desc = "Git [H]unk" },
 		}, { mode = "v" })
 	end,
 }

4ba59f4573360e14432341f574c7858ce912f4ed
Nick Coffey <nicholasjcoffey@gmail.com>
2025-03-20 14:47:40 -0500

    (nvim) updated theme to catppuccin

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,10 +2,10 @@ return {
 	"nvim-lualine/lualine.nvim",
 	opts = {
 		options = {
-			icons_enabled = true,
-			theme = "onedark",
+			icons_enabled = false,
+			theme = "catppuccin",
 			component_separators = "|",
-			section_separators = "",
+			section_separators = { left = "", right = "" },
 		},
 	},
 }
